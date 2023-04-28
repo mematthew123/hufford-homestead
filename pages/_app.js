99be6c5f367e3d0f6bdddd3c358a34e67e8dda7e
Matthew Rhoads <mematthew123@gmail.com>
2023-04-28 12:17:23 -0600

    anlytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import Nav from '@/components/Nav'
+import "@/styles/globals.css";
+import Nav from "@/components/Nav";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-  return <><Nav /><Component {...pageProps} /></>
+  return (
+    <>
+      <Nav />
+      <Component {...pageProps} />
+      <Analytics/>
+    </>
+  );
 }

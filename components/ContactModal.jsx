5c3271e1e201a72d100a354ffb5b2116f593b0d4
Matthew Rhoads <mematthew123@gmail.com>
2023-04-29 12:03:40 -0600

    commented out phone nnumber, updated maps location

--- components/ContactModal.jsx
+++ components/ContactModal.jsx
@@ -12,28 +12,28 @@ const ContactModal = ({ modalOpen, toggleModal }) => {
           onClick={toggleModal}
           className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
         ></div>
-        <div className="inline-block bg-white rounded-lg p-6 text-left overflow-hidden shadow-xl transform transition-all">
+        <div className="inline-block bg-gray-50 rounded-lg p-6 text-left overflow-hidden shadow-xl transform transition-all">
           <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
           <p>
             To get in touch with us, you can either call or send us an email.
           </p>
           <div className="flex flex-col mt-4">
-            <a
+            {/* <a
               href="tel:+1234567890"
               className="bg-blue-700 text-white py-2 px-4 rounded mb-2 hover:bg-blue-800"
             >
               Call: +1 (234) 567-890
-            </a>
+            </a> */}
             <a
               href="mailto:huffordhomestead@gmail.com"
-              className="bg-blue-700 text-white py-2 px-4 rounded hover:bg-blue-800"
+              className="bg-gray-700 text-white py-2 px-4 rounded hover:bg-blue-800"
             >
               Email: huffordhomestead@gmail.com
             </a>
           </div>
           <button
             onClick={toggleModal}
-            className="bg-red-500 text-white py-2 px-4 rounded mt-4 hover:bg-red-600"
+            className="bg-[#ccd5aeff] text-white py-2 px-4 rounded mt-4 hover:bg-red-600"
           >
             Close
           </button>
--- components/Locate.jsx
+++ components/Locate.jsx
@@ -7,14 +7,14 @@ export function Locate() {
         <div className="md:text-center">
           <h2 className="font-display text-3xl tracking-tight text-slate-50 sm:text-4xl"></h2>
           <p className=" text-lg text-slate-400">
-            123 W Moore st, Glens Ferry ID
+            608 Moore Ave, Glens Ferry ID
           </p>
         </div>
 
         <div className="relative mt-4 overflow-hidden bg-slate-50 py-2 px-2">
           <div className="aspect-w-16 aspect-h-12">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2727.956337842168!2d-114.08454862366494!3d46.864231638946485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x535dd1f271f93a55%3A0xfd6c7cd0b498686d!2s715%20Clements%20Rd%2C%20Missoula%2C%20MT%2059804!5e0!3m2!1sen!2sus!4v1682527650766!5m2!1sen!2sus"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1537.8836094641126!2d-115.30904436116461!3d42.959568955720954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54ac46f31f31029d%3A0xc865045391e00d82!2s608%20Moore%20Ave%2C%20Glenns%20Ferry%2C%20ID%2083623!5e1!3m2!1sen!2sus!4v1682791014635!5m2!1sen!2sus"
               style={{
                 width: "100%",
                 height: "300px",
